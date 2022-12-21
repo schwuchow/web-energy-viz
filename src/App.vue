96f2284cb0d2852dad801e3bfd76e0e0f399e34b
Judali <julia.schwuchow@lab25.de>
2022-12-21 18:03:07 +0100

    add example visualization with d3, add appliances

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="grid">
-  <div class="video-feed col-2">
+  <div class="video-feed col-3">
     Video Feed
   </div>
-  <div class="voice col-8">
+  <div class="voice col-7">
     <div class="voice__bar">
       <span>
         <img :src="voiceWaveImg" id="voice__icon-wave" class="icon" />
@@ -15,10 +15,30 @@
     <h2>Instructions</h2>
     <div class="instructions__text">Say "Hi, my home" to evoke speech</div>
   </div>
-  <div class="legend col-2">
+  <div class="appliances col-2">
     <h2>Appliances</h2>
+    <div class="appliances__all-devices">
+      <div>
+        <img :src="houseImg" id="appliances__house-icon" class="icon" />
+        <p class="appliances__house-text">All Devices</p>
+      </div>
+      <div class="appliances__house-toggle">
+        <p>Un<wbr>selected</p>
+        <div class="appliances__house-toggle-nr">10</div>
+      </div>
+    </div>
+    <div class="appliances__all-devices selected">
+      <div>
+        <img :src="houseImg" id="appliances__house-icon" class="icon" />
+        <p class="appliances__house-text">All Devices</p>
+      </div>
+      <div class="appliances__house-toggle">
+        <p>Un<wbr>selected</p>
+        <div class="appliances__house-toggle-nr">10</div>
+      </div>
+    </div>
   </div>
-  <div class="apartment col-8">
+  <div class="apartment col-7">
     <div class="apartment__img-container">
       <object :data="apartmentImg" type="image/svg+xml" id="apartment__img" width="100%" height="100%" ref="apartment"></object>
       <div id="tooltip-lamp1" :class="{ tooltip: true, show: livingRoomLamp1Focused }">Tooltip for Lamp</div>
@@ -26,19 +46,22 @@
       <div id="tooltip-table" :class="{ tooltip: true, show: tableFocused }">Tooltip for Table</div>
     </div>
   </div>
-  <div class="sidebar col-2">
+  <div class="sidebar col-3">
     <h3>Data Visualization</h3>
-    <div class="sidebar__energy-overview"></div>
-    <div class="sidebar__energy-viz-1"></div>
-    <div class="sidebar__energy-viz-2"></div>
+    <div class="sidebar__energy-viz">
+      <svg class="sidebar__energy-viz-svg" width="370" height="400"></svg>
+    </div>
   </div>
 </div>
 </template>
 
 <script lang="ts">
 import { onMounted, ref, VNodeRef, Ref } from 'vue';
+import * as d3 from "d3";
 import voiceWaveImg from './assets/voice_wave.svg';
 import apartmentImg from './assets/apartment.svg';
+import houseImg from './assets/house.svg';
+import refrigeratorData from './datasets/data.tsv';
 
 export default {
     setup() {
@@ -56,6 +79,8 @@ export default {
 
         addWebGazeListener();
         setTimeout(() => getDevices(), 5000);
+
+        showVisualization();
       })
 
       const getDevices = (): void => {
@@ -125,7 +150,66 @@ export default {
           yPred >= device.y && yPred <= (device.y + device.height);
       };
 
-      return { voiceWaveImg, livingRoomLamp1Focused, apartmentImg, apartment, tableFocused, sofaFocused };
+      const showVisualization = () => {
+        console.log(refrigeratorData);
+        refrigeratorData.forEach((d: any) => {
+          d.timestamp = new Date(d.timestamp).getSeconds();
+        });
+
+        var svg = d3.select(".sidebar__energy-viz-svg"),
+        margin = 100,
+        width = parseInt(svg.attr("width")) - margin,
+        height = parseInt(svg.attr("height")) - margin;
+
+
+        var xScale = d3.scaleBand().range ([0, width]).padding(0.4),
+            yScale = d3.scaleLinear().range ([height, 0]);
+
+        var g = svg.append("g")
+                  .attr("transform", "translate(" + 50 + "," + 50 + ")");
+
+        xScale.domain(refrigeratorData.map(function(d: any) { return d.timestamp; }));
+        yScale.domain([60, d3.max(refrigeratorData as Iterable<any>, function(d: any) { return d.energy; })]);
+
+        g.append("g")
+         .attr("transform", "translate(0," + height + ")")
+         .call(d3.axisBottom(xScale));
+
+         g.selectAll(".bar")
+         .data(refrigeratorData)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d: any) { return xScale(d.timestamp); })
+         .attr("y", function(d: any) { return yScale(d.energy); })
+         .attr("width", xScale.bandwidth())
+         .attr("height", function(d: any) { return height - yScale(d.energy); })
+         .attr("fill", "#A5A9FF");
+
+        g.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(xScale))
+          .append("text")
+          .attr("y", height - 260)
+          .attr("x", width - 100)
+          .attr("text-anchor", "end")
+          .attr("stroke", "#2E0B49")
+          .text("Timestamp");
+
+        g.append("g")
+          .call(d3.axisLeft(yScale)
+          .tickFormat(function(d: any) {
+              return d;
+          }).ticks(10))
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "-4.1em")
+          .attr("text-anchor", "end")
+          .attr("stroke", "#2E0B49")
+          .text("Energy");
+      };
+
+      return { voiceWaveImg, livingRoomLamp1Focused, apartmentImg, houseImg, apartment, tableFocused, sofaFocused };
     },
 };
 </script>
@@ -228,6 +312,7 @@ export default {
 
       .instructions__text {
         background-color: var(--color-light);
+        color: var(--color-primary);
         border: 2px solid var(--color-primary);
         border-radius: 10px;
         padding: 10px;
@@ -235,10 +320,77 @@ export default {
       }
     }
 
-    .legend {
+    .appliances {
       grid-row: span 5;
       order: 1;
       border-color: transparent;
+
+      .appliances__all-devices {
+        background-color: var(--color-secondary);
+        border-radius: 10px;
+        height: 130px;
+        display: flex;
+        padding: 20px;
+        width: calc(100% - 60px);
+        margin: auto;
+
+        .appliances__house-text {
+          color: var(--color-light);
+          text-transform: uppercase;
+          font-weight: bold;
+        }
+
+        .appliances__house-toggle {
+          height: 100%;
+          width: 50%;
+          border-radius: 10px;
+          background: rgba(250,250,250,0.3);
+          display: flex;
+          flex-direction: column;
+          margin-left: 10px;
+
+          p {
+            text-transform: uppercase;
+            margin-bottom: 5px;
+            font-size: 15px;
+          }
+
+          .appliances__house-toggle-nr {
+            width: calc(100% - 20px);
+            height: 50px;
+            background-color: var(--color-light);
+            font-weight: bold;
+            color: #A5A9FF;
+            margin: 0 auto;
+            margin-bottom: 10px;
+            line-height: 50px;
+            border-radius: 10px;
+          }
+        }
+      }
+
+      .selected {
+        &.appliances__all-devices {
+          background-color: #F7B634;
+
+          .appliances__house-text {
+            color: var(--color-secondary);
+          }
+
+          .appliances__house-icon {
+            fill: var(--color-secondary);
+          }
+
+          .appliances__house-toggle {
+            flex-direction: column-reverse;
+            .appliances__house-toggle-nr {
+              margin: 0 auto;
+              margin-bottom: 0;
+              margin-top: 10px;
+            }
+          }
+        }
+      }
     }
 
     .apartment {
@@ -298,14 +450,19 @@ export default {
     .sidebar {
       grid-row: span 5;
       border-color: transparent;
+      margin-top: 40px;
 
-      &__energy-viz-1,
-      &__energy-viz-2 {
+      .sidebar__energy-viz {
         background-color: var(--color-light);
         border: 1px solid transparent;
         border-radius: 10px;
-        height: 230px;
+        height: 100%;
         margin: 20px;
+        position: relative;
+      }
+      
+      .sidebar__energy-viz-svg {
+        z-index: 5;
       }
     }
 }
